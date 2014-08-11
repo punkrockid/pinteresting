3659546b116c0833d30541b644d586fb327da7ce
punkrockid <israel.valdez@teschglobal.com>
2014-08-11 11:35:28 -0500

    amazon s3 for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,4 +82,15 @@ Rails.application.configure do
 
   # required for heroku. set to host name
   config.action_mailer.default_url_options = { host: '0.0.0.0' }
+
+  config.paperclip_defaults =
+  {
+    :storage => :s3,
+    :s3_credentials =>
+    {
+      :bucket => ENV['AWS_BUCKET'],
+      :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
+    }
+  }
 end
